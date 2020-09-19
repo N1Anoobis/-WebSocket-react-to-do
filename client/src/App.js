a61648cfb480cebd37159618cb25d315f90c6e7e
N1Anoobis <mrbond84@hotmail.com>
2020-09-19 16:19:21 +0200

    Add edit option and fix removing bug

--- client/src/App.js
+++ client/src/App.js
@@ -29,22 +29,31 @@ class App extends React.Component {
   }
 
   submitForm(e) {
-    const {  taskName } = this.state;
+    const { taskName } = this.state;
     e.preventDefault()
     const id = uuidv4();
     const newTask = { id: id, name: taskName };
     this.addTask(newTask);
     this.socket.emit('addTask', newTask);
-    this.setState({taskName: ''});
+    this.setState({ taskName: '' });
   }
 
   removeTask(id, localAction) {
     const { tasks } = this.state;
     this.setState({ tasks: tasks.filter(task => task.id !== id) });
-    if(localAction){
-    this.socket.emit('removeTask', id);
-    console.log('sending removeTask to server only when is done localy first')
+    if (localAction) {
+      this.socket.emit('removeTask', id);
+      console.log('sending removeTask to server only when is done localy first')
+    }
   }
+
+  changeTaskName(value, id) {
+    const { tasks } = this.state;
+    let currentTask = tasks.find(task => task.id === id);
+    console.log(currentTask.name);
+    currentTask.name = value
+    this.setState({ currentTask });
+    this.socket.emit('updateData', tasks);
   }
 
   render() {
@@ -55,9 +64,11 @@ class App extends React.Component {
           <h1>ToDoList.app</h1>
         </header>
         <section className="tasks-section" id="tasks-section">
-          <h2>Tasks</h2>
+          <h2>You can edit Tasks</h2>
           <ul className="tasks-section__list" id="tasks-list">
-            {tasks.map(task => (<li key={task.id} className="task">{task.name}<button className="btn btn--red" onClick={() => this.removeTask(task.id, true)}>Remove</button></li>))}
+            {tasks.map(task => (<li key={task.id} className="task">
+              <input className='input' type="text" value={task.name} onChange={(e) => this.changeTaskName(e.target.value, task.id)} />
+              <button className="btn btn--red" onClick={() => this.removeTask(task.id, true)}>Remove</button></li>))}
           </ul>
           <form id="add-task-form" onSubmit={(e) => this.submitForm(e)}>
             <input className="text-input" autoComplete="off" type="text" placeholder="Type your description" id="task-name" value={taskName} onChange={(e) => this.updateTaskName(e.target.value)} />
--- server.js
+++ server.js
@@ -5,23 +5,27 @@ const app = express()
 app.use(cors());
 
 const server = app.listen(8000, () => {
-  
+
 });
 const io = socket(server);
 
 let tasks = [{
-  id: 1,
-  name: 'Prorgraming'
-},
-{
-  id: 2,
-  name: 'Go out'
-}
+    id: 1,
+    name: 'Prorgraming'
+  },
+  {
+    id: 2,
+    name: 'Go out'
+  }
 ];
 
 io.on('connection', (socket) => {
 
   socket.emit('updateData', tasks)
+  socket.on('updateData', (updatedTasks) => {
+    tasks = updatedTasks
+    socket.broadcast.emit('updateData', tasks);
+  });
 
   socket.on('addTask', (task) => {
     tasks.push(task);
@@ -29,7 +33,7 @@ io.on('connection', (socket) => {
   });
   socket.on('removeTask', (index) => {
     tasks.filter(task => {
-      task.id != index
+      task.id === index
     })
     socket.broadcast.emit('removeTask', index);
   });
